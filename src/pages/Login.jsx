017ba8119dde9eb0302978db3fcdc15711377479
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-04-22 13:24:21 +0300

    style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div className='loginDiv'>
-      <div className='h-[500px] w-11/12 sm:w-[475px] bg-white'>
-
+    <div className="loginDiv">
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col items-center">
+        <div className="flex justify-center items-center mt-2 gap-2">
+          <span className="w-[6px] h-[39px]  bg-yellow-500"></span>
+          <h1
+            className="text-[25px] sm:text-[32px] font-montserrat font-[700]
+ uppercase"
+          >
+            Clarus Store
+          </h1>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
