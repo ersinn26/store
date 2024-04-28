68d6f1bb3fca744dedfea9c76d30e5645d5e9fb8
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-04-28 22:59:52 +0300

    Login-1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <div className="loginDiv">
-      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col items-center">
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col items-center text-center">
         <div className="flex justify-center items-center mt-2 gap-2">
           <span className="w-[6px] h-[39px]  bg-yellow-500"></span>
           <h1
@@ -12,6 +12,10 @@ const Login = () => {
           >
             Clarus Store
           </h1>
+        </div>
+        <div className="my-2">
+          <h3 className="text-[22px] font-[600] font-montserrat">SIGN IN</h3>
+          <p className="text-labelColor font-label font-montserrat mt-1">Enter your credentials to access your account</p>
         </div>
       </div>
     </div>
